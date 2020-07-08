01495b2189c9cfd8041484b3f8f7f09eb573dd05
Stefaan <safwebdev@gmail.com>
2020-07-09 03:09:10 +0800

    > update create education page > install react image resizer > Refactor Education Section

--- src/components/education/CreateEducation.js
+++ src/components/education/CreateEducation.js
@@ -1,242 +1,166 @@
-import React, { useEffect, useState } from "react";
+import React, { Component } from "react";
 import { createEducation } from "../../store/actions/eduActions";
 import { connect } from "react-redux";
-import { Container, Grid, Button } from "@material-ui/core";
-import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
-import { useForm } from "react-hook-form";
-import { storage } from "../../config/config";
 import { compose } from "redux";
-import Avatar from "@material-ui/core/Avatar";
-import LinearProgress from "@material-ui/core/LinearProgress";
+import Resizer from "react-image-file-resizer";
+import {
+  Container,
+  Grid,
+  Button,
+  Typography,
+  TextField,
+} from "@material-ui/core";
+class CreateEducation extends Component {
+  constructor(props) {
+    super(props);
+    this.fileChangedHandler = this.fileChangedHandler.bind(this);
+    this.state = {
+      image: null,
+      url: "",
+      photoID: "",
+      progress: 0,
+      name: "",
+      field: "",
+      location: "",
+      startYear: null,
+      endYear: null,
+    };
+  }
 
-const CreateEducation = (props) => {
-  // image upload
-  const [image, setImage] = useState(null);
-  const [url, setUrl] = useState("");
-  const [photoID, setphotoID] = useState("");
-  const [progress, setProgress] = useState(0);
-
-  // form stuff
-  const { register, handleSubmit, errors } = useForm();
-
-  const handleChange = (e) => {
-    if (e.target.files[0]) {
-      setImage(e.target.files[0]);
+  fileChangedHandler(event) {
+    var fileInput = false;
+    if (event.target.files[0]) {
+      fileInput = true;
     }
-  };
-
-  useEffect(() => {});
-
-  const handleUpload = () => {
-    let type = "";
-    let d = new Date(),
-      filename =
-        [d.getFullYear(), d.getMonth() + 1, d.getDate()].join("") +
-        [d.getHours(), d.getMinutes(), d.getSeconds()].join("");
-    let noError = true;
-
-    switch (image.type) {
-      case "image/jpeg":
-        type = ".jpeg";
-        noError = true;
-        break;
-      case "image/jpg":
-        type = ".jpg";
-        noError = true;
-        break;
-      case "image/png":
-        type = ".png";
-        noError = true;
-        break;
-      default:
-        noError = false;
-    }
-
-    if (noError) {
-      const imageFile = filename + type;
-      const imageUrl = "images/education/";
-      const fullUrl = imageUrl + imageFile;
-      const uploadTask = storage.ref(fullUrl).put(image);
-
-      setphotoID(filename);
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          const progress = Math.round(
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-          );
-          setProgress(progress);
+    if (fileInput) {
+      Resizer.imageFileResizer(
+        event.target.files[0],
+        700,
+        700,
+        "JPEG",
+        100,
+        0,
+        (uri) => {
+          this.setState({ image: uri });
         },
-        (error) => {
-          console.log(error);
-        },
-        () => {
-          storage
-            .ref(imageUrl)
-            .child(imageFile)
-            .getDownloadURL()
-            .then((url) => {
-              setUrl(url);
-              setImage(null);
-              setProgress(0);
-            });
-        }
+        "base64"
       );
-    } else {
-      alert("Invalid file format. Please use Jpg, Jpeg or Pngs");
     }
+  }
+  handleChange = (e) => {
+    this.setState({ [e.target.id]: e.target.value });
   };
-
-  const onSubmit = (data, e) => {
+  handleSubmit = (e) => {
     e.preventDefault();
-    console.log("SUBMIT");
-    console.log(data);
-    if (url) {
-      props.createEducation(data, url, photoID);
-      props.history.push("/");
+    if (this.state.image) {
+      alert("UPLADED");
+      console.log(this.state);
+      // props.createEducation(data, url, photoID);
+      // props.history.push("/");
     } else {
-      alert("Please Upload an image");
+      alert("Please upload an image.");
     }
   };
-  return (
-    <Container maxWidth="lg">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Grid container spacing={3}>
-          <Grid xs={12} />
-          <Grid item xs={12}>
-            <Typography variant="h5" component="h5">
-              Education Image
-            </Typography>
-          </Grid>
-          <Grid item sm={4}>
-            <Avatar
-              className="profile-img"
-              variant="square"
-              src={
-                url
-                  ? url
-                  : "https://via.placeholder.com/400x400?text=Profile+Image"
-              }
-            />
-          </Grid>
-          <Grid item sm={8}>
-            {progress >= 1 ? (
-              <LinearProgress variant="determinate" value={progress} />
-            ) : null}
-            <br />
-            <input type="file" onChange={handleChange} />
-            <br />
-            <br />
-            {image ? (
-              <Button variant="contained" onClick={handleUpload}>
-                Upload
-              </Button>
-            ) : (
-              <Button variant="contained" disabled>
-                Upload
-              </Button>
-            )}
-            <br />
-            <br />
-          </Grid>
-          <Grid item xs={12}>
-            <Typography variant="h5" component="h5">
-              Add New Education
-            </Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              id="name"
-              type="name"
-              name="name"
-              autoComplete="name"
-              label="School Name"
-              variant="outlined"
-              errors={!!errors.name}
-              inputRef={register({
-                required: true,
-              })}
-              required
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              id="field"
-              type="field"
-              name="field"
-              autoComplete="field"
-              label="Field"
-              variant="outlined"
-              errors={!!errors.field}
-              inputRef={register({
-                required: true,
-              })}
-              required
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              id="location"
-              type="location"
-              name="location"
-              autoComplete="location"
-              label="Location"
-              variant="outlined"
-              errors={!!errors.location}
-              inputRef={register({
-                required: true,
-              })}
-              required
-              fullWidth
-            />
-          </Grid>
 
-          <Grid item xs={12} sm={6}>
-            <TextField
-              id="startYear"
-              type="number"
-              name="startYear"
-              autoComplete="startYear"
-              label="Start Year"
-              variant="outlined"
-              errors={!!errors.startYear}
-              inputRef={register({
-                required: true,
-              })}
-              required
-              fullWidth
-            />
+  render() {
+    const { image } = this.state;
+    return (
+      <Container maxWidth="lg">
+        <form onSubmit={this.handleSubmit}>
+          <Grid container spacing={3}>
+            <Grid xs={12} />
+            <Grid item xs={12}>
+              <Typography variant="h5" component="h5">
+                Education Image
+              </Typography>
+            </Grid>
+            <Grid item sm={4}>
+              {image ? <img src={image} alt="" /> : null}
+            </Grid>
+            <Grid item sm={8}>
+              <input type="file" onChange={this.fileChangedHandler} />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h5" component="h5">
+                Add New Education
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                id="name"
+                type="name"
+                name="name"
+                autoComplete="name"
+                label="School Name"
+                variant="outlined"
+                onChange={this.handleChange}
+                required
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                id="field"
+                type="field"
+                name="field"
+                autoComplete="field"
+                label="Field"
+                variant="outlined"
+                onChange={this.handleChange}
+                required
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                id="location"
+                type="location"
+                name="location"
+                autoComplete="location"
+                label="Location"
+                variant="outlined"
+                onChange={this.handleChange}
+                required
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                id="startYear"
+                type="number"
+                name="startYear"
+                autoComplete="startYear"
+                label="Start Year"
+                variant="outlined"
+                onChange={this.handleChange}
+                required
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                id="endYear"
+                type="number"
+                name="endYear"
+                autoComplete="endYear"
+                label="End Year"
+                variant="outlined"
+                onChange={this.handleChange}
+                required
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button type="submit" variant="contained">
+                Submit
+              </Button>
+            </Grid>
           </Grid>
-
-          <Grid item xs={12} sm={6}>
-            <TextField
-              id="endYear"
-              type="number"
-              name="endYear"
-              autoComplete="endYear"
-              label="End Year"
-              variant="outlined"
-              errors={!!errors.endYear}
-              inputRef={register({
-                required: true,
-              })}
-              required
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button type="submit" variant="contained">
-              Submit
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
-    </Container>
-  );
-};
+        </form>
+      </Container>
+    );
+  }
+}
 
 const mapDispatchToProps = (dispatch) => {
   return {
--- src/components/education/EducationSection.js
+++ src/components/education/EducationSection.js
@@ -1,13 +1,8 @@
 import React from "react";
-import Chip from "@material-ui/core/Chip";
 import { firestoreConnect } from "react-redux-firebase";
 import { connect } from "react-redux";
 import { compose } from "redux";
-import Typography from "@material-ui/core/Typography";
-import { Grid } from "@material-ui/core";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Avatar from "@material-ui/core/Avatar";
+import { Grid, Typography, Card, CardContent, Avatar } from "@material-ui/core";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -49,34 +44,23 @@ const EducationSection = (props) => {
     ],
   };
 
-  const ShowEducation = ({ title, skillType, styleCclass }) => {
+  const EducationBox = ({ data }) => {
+    const { photoUrl, endYear, field, name, location } = data;
     return (
-      <>
-        {education &&
-          education.map((data) => {
-            return (
-              <div className="edu-box">
-                lol
-                {/* <Card>
-                  <CardContent className="edu-content">
-                    <Avatar alt="" src={data.photoUrl} className="edu-img" />
-                    <div className="block">
-                      <Typography component="h6" variant="h6">
-                        {data.endYear} | {data.field}
-                      </Typography>
-                      <Typography variant="subtitle1" variant="subtitle1">
-                        {data.name}
-                      </Typography>
-                      <Typography variant="subtitle2" variant="subtitle2">
-                        {data.location}
-                      </Typography>
-                    </div>
-                  </CardContent>
-                </Card> */}
-              </div>
-            );
-          })}
-      </>
+      <div className="edu-box">
+        <Card>
+          <CardContent className="edu-content">
+            <Avatar alt="" src={photoUrl} className="edu-img" />
+            <div className="block">
+              <Typography component="h6" variant="h6">
+                {endYear} | {field}
+              </Typography>
+              <Typography variant="subtitle1">{name}</Typography>
+              <Typography variant="subtitle2">{location}</Typography>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     );
   };
 
@@ -89,58 +73,11 @@ const EducationSection = (props) => {
       </Grid>
       <Grid item xs={12} className="">
         <Slider {...settings}>
-          {/* <ShowEducation /> */}
-
           {education &&
-            education.map((data) => {
-              return (
-                <div className="edu-box">
-                  <Card>
-                    <CardContent className="edu-content">
-                      <Avatar alt="" src={data.photoUrl} className="edu-img" />
-                      <div className="block">
-                        <Typography component="h6" variant="h6">
-                          {data.endYear} | {data.field}
-                        </Typography>
-                        <Typography variant="subtitle1" variant="subtitle1">
-                          {data.name}
-                        </Typography>
-                        <Typography variant="subtitle2" variant="subtitle2">
-                          {data.location}
-                        </Typography>
-                      </div>
-                    </CardContent>
-                  </Card>
-                </div>
-              );
+            education.map((data, index) => {
+              return <EducationBox key={index} data={data} />;
             })}
         </Slider>
-        {/* <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-          <div>
-            <h3>7</h3>
-          </div>
-          <div>
-            <h3>8</h3>
-          </div>
-        </Slider> */}
       </Grid>
     </Grid>
   );
